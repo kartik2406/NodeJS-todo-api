0174dff0ea30ee30353bc5bb24e315d9a3fb2b16
kartik2406 <kartikmalik91@gmail.com>
2017-01-28 18:20:34 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {user} = require('./models/user');
+var {User} = require('./models/user');
 
 var app = express();
 
@@ -24,10 +25,8 @@ app.post('/todos', (request, response) => {
     });
 });
 
-app.listen(3000, () => {
-   console.log('Started on port 3000') ;
-});
 
+//Get /todos
 app.get('/todos', (request, response) => {
    Todo.find().then( (todos) => {
        response.send({
@@ -36,6 +35,29 @@ app.get('/todos', (request, response) => {
    }, (e) => {
        response.staus(400).send(e);
    }) 
+});
+
+//Get /todos/id
+app.get('/todos/:id', (request, response) => {
+    var id = request.params.id;
+    if(!ObjectID.isValid(id)){
+        return response.status(404).send();
+    }
+    
+    Todo.findById(id).then( (todo) => {
+        if(!todo){
+            return response.status(404).send();
+        }
+        response
+            .status(200)
+            .send({todo});
+    }, (e) => {
+        response.status(400).send();
+    });
+});
+
+app.listen(3000, () => {
+   console.log('Started on port 3000') ;
 });
 
 module.exports = {app};
