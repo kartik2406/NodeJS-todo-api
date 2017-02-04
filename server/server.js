cbd7677b876b27d282cb21860fb13084adc413dc
kartik2406 <kartikmalik91@gmail.com>
2017-02-04 13:04:23 +0530

    Add GET /users/me

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -116,6 +117,12 @@ app.post('/users', (request, response) => {
         response.status(400).send(e);
     });
 });
+
+
+
+app.get('/users/me', authenticate, (request, response) =>{
+  response.send(request.user) ;
+})
 app.listen(port, () => {
    console.log(`Started on port ${port}`) ;
 });
